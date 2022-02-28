e5ab1ab82e3fd69db8d3536457803b7743a886b2
ankit1-salvi <ankit.salvi@crubn.com>
2022-02-28 16:34:09 +0530

    bottom margin corrected

--- src/App/styles..ts
+++ src/App/styles..ts
@@ -2,10 +2,8 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles({
   root: {
-    height: "125vh",
-    overflow: "hidden",
     position: "relative",
-    marginBottom:'12%'
+    marginBottom:'20%'
   },
 });
 
