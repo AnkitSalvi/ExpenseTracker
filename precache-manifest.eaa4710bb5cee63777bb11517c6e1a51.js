741f2c6ffcbdfda4ae3e3ff4ace40fdde8cea7d6
ankit1-salvi <ankit.salvi@crubn.com>
2022-02-28 14:50:03 +0530

    Updates

---
+++ precache-manifest.eaa4710bb5cee63777bb11517c6e1a51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c7eb29ece07460a61b7517a2dcf6b1",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "b3e608d6e3a5a70fa78e",
+    "url": "/expense-tracker/static/js/2.0e8443f2.chunk.js"
+  },
+  {
+    "revision": "24c4b50c905088b8444db3c884349cac",
+    "url": "/expense-tracker/static/js/2.0e8443f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8decf9c0cd02aec167aa",
+    "url": "/expense-tracker/static/js/main.3a1acbc4.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
